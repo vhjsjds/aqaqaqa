cd56287aac1e67d29a6c00c8e78934c902fafceb
yel <>
2025-09-25 17:53:41 +0200

    ²

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    sourcemap: false, // Désactive les source maps en production pour minimiser les informations visibles
+  },
+});
